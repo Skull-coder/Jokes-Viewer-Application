e669f6395786acd74ad0a024043b03d658d456af
Skull-coder <skullthecoder@gmail.com>
2026-05-03 19:04:42 +0530

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [joke, setJoke] = useState(0)
+  const [joke, setJoke] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
